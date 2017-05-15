132afa3082fa6f4fa4ba6491459db6f181ba61fb
Simon Driesen <simon.driesen@hotmail.com>
2017-05-16 00:49:39 +0200

    Cleaner and more simple way to disable alerts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+/* ---------------------------------------- Disabling "Confirm exit" pop ups ---------------------------------------- */
+
 // Script that will be inserted into web page
 var disablerFunction = function () {
     setInterval(function () {
         window.onbeforeunload = null;
     }, 50);
+    window.alert = function alert (message) {
+        console.info ("alert: " + message);
+    }
 };
 
 // Making a string of the function
@@ -11,30 +16,10 @@ var disablerCode = "(" + disablerFunction.toString() + ")();";
 // Creating a script DOM element and putting disablerFunction in it
 var disablerScriptElement = document.createElement('script');
 disablerScriptElement.textContent = disablerCode;
+disablerScriptElement.type = "text/javascript";
 
 // Inserting script into html element
 document.documentElement.appendChild(disablerScriptElement);
 
 // Removing script since it has executed already
 disablerScriptElement.parentNode.removeChild(disablerScriptElement);
-
-// Blocks javascript alerts
-addJS_Node (null, null, overrideSelectNativeJS_Functions);
-
-function addJS_Node (text, s_URL, funcToRun) {
-    var D                                   = document;
-    var scriptNode                          = D.createElement ('script');
-    scriptNode.type                         = "text/javascript";
-    if (text)       scriptNode.textContent  = text;
-    if (s_URL)      scriptNode.src          = s_URL;
-    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
-
-    var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
-    targ.appendChild (scriptNode);
-}
-
-function overrideSelectNativeJS_Functions () {
-    window.alert = function alert (message) {
-        console.log (message);
-    }
-}
