34440ffd07781b35b63fb538e47b504e195843c8
Simon Driesen <simon.driesen@hotmail.com>
2017-05-15 18:53:53 +0200

    Exit Tab will now block confirm pop ups if the page doesn't block js insertions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function() {
-    console.log('INSERT DOM JS HERE');
+    // Script that will be inserted into web page
+    var disablerFunction = function () {
+        window.alert = function alert(msg) { console.log('Hidden Alert ' + msg); };
+        window.onbeforeunload = null;
+    };
+
+    // Making a string of the function
+    var disablerCode = "(" + disablerFunction.toString() + ")();";
+
+    // Creating a script DOM element and putting disablerFunction in it
+    var disablerScriptElement = document.createElement('script');
+    disablerScriptElement.textContent = disablerCode;
+
+    // Inserting script into html element
+    document.documentElement.appendChild(disablerScriptElement);
+
+    // Removing script since it has executed already
+    disablerScriptElement.parentNode.removeChild(disablerScriptElement);
 });
