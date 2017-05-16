dd851538178d2549f1191fde53c5e0a8d7b29992
Simon Driesen <simon.driesen@hotmail.com>
2017-05-16 20:23:02 +0200

    Made code more DRY

--- background.js
+++ background.js
@@ -1,52 +1,33 @@
 if (localStorage.getItem("toggle") === undefined || localStorage.getItem("toggle") === null) {
     localStorage.setItem("toggle", "true");
-    console.log('SETTING KEY');
 }
-
-console.log('start, toggle =' + localStorage.getItem("toggle"));
 
 chrome.browserAction.onClicked.addListener(function (tab) {
     var toggle = localStorage.getItem("toggle");
-    if (localStorage.getItem("toggle") == "false") {
+    if (toggle == "false") {
         console.log('SETON');
-
-
-        chrome.browserAction.setIcon({
-            path: "img/on.png",
-            tabId: tab.id
-        });
-        chrome.tabs.executeScript(tab.id, {
-            file: "js/main.js"
-        });
         localStorage.setItem("toggle", "true");
-    } else if (localStorage.getItem("toggle") == "true") {
+        updateIcon(tab);
+    } else if (toggle == "true") {
         console.log('SETOFF');
-        chrome.browserAction.setIcon({
-            path: "img/off.png",
-            tabId: tab.id
-        });
-        chrome.tabs.executeScript(tab.id, {
-            code: ""
-        });
         localStorage.setItem("toggle", "false");
+        updateIcon(tab);
     }
 });
 
+chrome.tabs.onUpdated.addListener(function (tab) {updateIcon(tab)});
+chrome.tabs.onActivated.addListener(function (tab) {updateIcon(tab)});
 
-chrome.tabs.onUpdated.addListener(function (tab) {
+function updateIcon(tab) {
     var toggle = localStorage.getItem("toggle");
-    if (localStorage.getItem("toggle") == "true") {
-        console.log('created ON ' + localStorage.getItem("toggle"));
-
-
+    if (toggle == "true") {
         chrome.browserAction.setIcon({
             path: "img/on.png"
         });
         chrome.tabs.executeScript(tab.id, {
             file: "js/main.js",
         });
-    } else if (localStorage.getItem("toggle") == "false") {
-        console.log('created OFF ' + localStorage.getItem("toggle"));
+    } else if (toggle == "false") {
         chrome.browserAction.setIcon({
             path: "img/off.png"
         });
@@ -54,51 +35,4 @@ chrome.tabs.onUpdated.addListener(function (tab) {
             code: "",
         });
     }
-})
-
-
-chrome.tabs.onSelectionChanged.addListener(function (tab) {
-    var toggle = localStorage.getItem("toggle");
-    if (localStorage.getItem("toggle") == "true") {
-        console.log('created ON ' + localStorage.getItem("toggle"));
-
-
-        chrome.browserAction.setIcon({
-            path: "img/on.png"
-        });
-        chrome.tabs.executeScript(tab.id, {
-            file: "js/main.js",
-        });
-    } else if (localStorage.getItem("toggle") == "false") {
-        console.log('created OFF ' + localStorage.getItem("toggle"));
-        chrome.browserAction.setIcon({
-            path: "img/off.png"
-        });
-        chrome.tabs.executeScript(tab.id, {
-            code: "",
-        });
-    }
-})
-
-chrome.tabs.onActivated.addListener(function (tab) {
-    var toggle = localStorage.getItem("toggle");
-    if (localStorage.getItem("toggle") == "true") {
-        console.log('created ON ' + localStorage.getItem("toggle"));
-
-
-        chrome.browserAction.setIcon({
-            path: "img/on.png"
-        });
-        chrome.tabs.executeScript(tab.id, {
-            file: "js/main.js",
-        });
-    } else if (localStorage.getItem("toggle") == "false") {
-        console.log('created OFF ' + localStorage.getItem("toggle"));
-        chrome.browserAction.setIcon({
-            path: "img/off.png"
-        });
-        chrome.tabs.executeScript(tab.id, {
-            code: "",
-        });
-    }
-})
+}
